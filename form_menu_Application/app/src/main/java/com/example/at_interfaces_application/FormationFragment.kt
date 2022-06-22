9f57a194b13ca5f14b79b51622d75b6d7127a404
Wellington Nascimento <wnsferreira@gmail.com>
2022-06-22 01:59:53 -0300

    app concluded

--- form_menu_Application/app/src/main/java/com/example/at_interfaces_application/FormationFragment.kt
+++ form_menu_Application/app/src/main/java/com/example/at_interfaces_application/FormationFragment.kt
@@ -6,14 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-
-class ProfessionalExperienceFragment : Fragment() {
+class FormationFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_professional_experience, container, false)
+        return inflater.inflate(R.layout.fragment_formation, container, false)
     }
+
+
 }
