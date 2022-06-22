a5e151569d420c1046018a6bcb3ad6663a38cb19
Wellington Nascimento <wnsferreira@gmail.com>
2022-06-22 00:29:27 -0300

    added navigation, menu and fragments

--- form_menu_Application/app/src/main/java/com/example/at_interfaces_application/MainActivity.kt
+++ form_menu_Application/app/src/main/java/com/example/at_interfaces_application/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.at_interfaces_application
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navController: NavController
+    lateinit var navView : NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //------------------------------------
+        drawerLayout = this.findViewById<DrawerLayout>(R.id.drawerLayout)
+        navController = this.findNavController(R.id.fragment)
+        navView = this.findViewById<NavigationView>(R.id.navView)
+        navView.setItemIconTintList(null)
+        //----------------------------------------------
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(navView, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
