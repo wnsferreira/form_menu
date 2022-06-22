fde1f25399971f610771c79987b27faccb5c4f77
Wellington Nascimento <wnsferreira@gmail.com>
2022-06-21 22:59:25 -0300

    created project

---
+++ form_menu_Application/app/src/main/java/com/example/at_interfaces_application/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.at_interfaces_application
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
